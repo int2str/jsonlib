3406dbfd9aaf4238d832af4d24b1f211a4b5a69e
Andre Eisenbach <github@4ae.us>
2021-07-25 22:14:15 -0700

    Cleaned up consumable.h based on clang-tidy suggestions

--- consumable.h
+++ consumable.h
@@ -4,53 +4,46 @@
 template <typename COLLECTION, typename ITEM>
 class Consumable {
  public:
-  explicit Consumable(const COLLECTION& collection)
-      : consumed_{}, collection_{collection} {}
+  explicit Consumable(const COLLECTION& collection) : collection_{collection} {}
 
   constexpr void reset() { consumed_ = 0; }
 
-  [[nodiscard]] constexpr bool eol() const {
-    return consumed_ >= collection_.size();
-  }
+  [[nodiscard]] bool eol() const { return consumed_ >= collection_.size(); }
 
-  [[nodiscard]] constexpr size_t size() const { return collection_.size(); }
+  [[nodiscard]] size_t size() const { return collection_.size(); }
 
-  [[nodiscard]] constexpr const COLLECTION& data() const { return collection_; }
+  [[nodiscard]] const COLLECTION& data() const { return collection_; }
 
-  [[nodiscard]] constexpr size_t idx() const { return consumed_; }
+  [[nodiscard]] size_t idx() const { return consumed_; }
 
-  [[nodiscard]] constexpr bool canPeek() const {
+  [[nodiscard]] bool canPeek() const {
     return (consumed_ < collection_.size() - 1);
   }
 
-  [[nodiscard]] constexpr const ITEM& peek() const {
+  [[nodiscard]] const ITEM& peek() const {
     return collection_.at(consumed_ + 1);
   }
 
-  [[nodiscard]] constexpr const ITEM& prev() const {
+  [[nodiscard]] const ITEM& prev() const {
     return collection_.at(consumed_ - 1);
   }
 
-  [[nodiscard]] constexpr const ITEM& current() const {
+  [[nodiscard]] const ITEM& current() const {
     return collection_.at(consumed_);
   }
 
   constexpr void skip(size_t items) { consumed_ += items; }
 
-  [[nodiscard]] constexpr const ITEM& consume() {
-    return collection_.at(consumed_++);
-  }
+  [[nodiscard]] const ITEM& consume() { return collection_.at(consumed_++); }
 
-  constexpr bool consume_if(const ITEM& compare) {
-    if (current() == compare) {
-      ++consumed_;
-      return true;
-    }
-    return false;
+  bool consume_if(const ITEM& compare) {
+    bool consume_item = current() == compare;
+    if (consume_item) ++consumed_;
+    return consume_item;
   }
 
  private:
-  size_t consumed_;
+  size_t consumed_{0};
   COLLECTION collection_;
 };
 
